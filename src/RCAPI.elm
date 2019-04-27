b031f80178ae2d332c6fdcc3c1f9e338210732fa
Luc Döbereiner <luc.doebereiner@gmail.com>
2019-04-28 00:23:10 +0200

    exposition  api

--- src/RCAPI.elm
+++ src/RCAPI.elm
@@ -2,19 +2,33 @@ module RCAPI exposing (getExposition, getMediaList)
 
 import Dict
 import Http
-import Json.Decode as D
+import Json.Decode exposing (..)
+
+
+type alias APIExposition =
+    { html : String, markdown : String, metadata : String, style : String, title : String }
+
+
+apiExposition : Decoder APIExposition
+apiExposition =
+    map5 APIExposition
+        (at [ "html" ] string)
+        (at [ "markdown" ] string)
+        (at [ "metadata" ] string)
+        (at [ "style" ] string)
+        (at [ "title" ] string)
 
 
 getMediaList id msg =
     Http.get
         { url = "/text-editor/simple-media-list?research=" ++ String.fromInt id
-        , expect = Http.expectJson msg (D.dict D.string)
+        , expect = Http.expectJson msg (dict string)
         }
 
 
-getExposition : Int -> Int -> (Result Http.Error (Dict.Dict String String) -> msg) -> Cmd msg
+getExposition : Int -> Int -> (Result Http.Error APIExposition -> msg) -> Cmd msg
 getExposition researchId weave msg =
     Http.get
         { url = " text-editor/load?research=" ++ String.fromInt researchId ++ "&weave=" ++ String.fromInt weave
-        , expect = Http.expectJson msg (D.dict D.string)
+        , expect = Http.expectJson msg apiExposition
         }
