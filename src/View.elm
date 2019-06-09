988c8affaa2e2b5021617a5e0b69b4c5ac2d8312
Luc Döbereiner <luc.doebereiner@gmail.com>
2019-06-09 15:46:04 +0200

    added View and icons

---
+++ src/View.elm
@@ -0,0 +1,114 @@
+module View exposing (Icon(..), mkButton, mkDropdown, renderIcon)
+
+import Bootstrap.Button as Button
+import Bootstrap.Dropdown as Dropdown
+import Bootstrap.Utilities.Spacing as Spacing
+import Html exposing (Html, a, button, div, img, li, p, span, text, ul)
+import Html.Attributes exposing (attribute, class, for, href, id, src)
+import Html.Events exposing (on, onCheck, onClick, onInput)
+import Settings exposing (iconUrl)
+
+
+type Icon
+    = PlusIcon
+    | ImportIcon
+    | SaveIcon
+    | ItalicIcon
+    | LinkIcon
+    | BoldIcon
+    | QuoteIcon
+    | HeaderIcon
+    | ListIcon
+    | NumberedIcon
+    | ArrowDown
+    | UploadCloud
+
+
+renderIcon : Icon -> Html msg
+renderIcon icon =
+    let
+        iconImg url =
+            img
+                [ src (iconUrl ++ url)
+                , class "m-1"
+                , Html.Attributes.width 15
+                , Html.Attributes.height 15
+                , Html.Attributes.style "position" "relative"
+                , Html.Attributes.style "top" "-2px"
+                ]
+                []
+    in
+    case icon of
+        PlusIcon ->
+            iconImg "plus.svg"
+
+        ImportIcon ->
+            iconImg "import-export.svg"
+
+        SaveIcon ->
+            iconImg "save.svg"
+
+        ItalicIcon ->
+            iconImg "italic.svg"
+
+        LinkIcon ->
+            iconImg "link-intact.svg"
+
+        BoldIcon ->
+            iconImg "bold.svg"
+
+        ListIcon ->
+            iconImg "list-unordered.svg"
+
+        NumberedIcon ->
+            iconImg "list-ordered.svg"
+
+        HeaderIcon ->
+            iconImg "header.svg"
+
+        QuoteIcon ->
+            iconImg "double-quote-sans-left.svg"
+
+        ArrowDown ->
+            iconImg "arrow-down.svg"
+
+        UploadCloud ->
+            iconImg "cloud-upload.svg"
+
+
+mkButton : Icon -> Bool -> msg -> String -> Html msg
+mkButton icon needsOffset onClickMsg buttonText =
+    let
+        spacing =
+            if needsOffset then
+                [ Spacing.m1 ]
+
+            else
+                []
+    in
+    Button.button
+        [ Button.light
+        , Button.attrs <| List.append [ onClick onClickMsg ] spacing
+        ]
+        [ renderIcon icon
+        , text buttonText
+        ]
+
+
+mkDropdown : Dropdown.State -> (Dropdown.State -> msg) -> String -> List ( String, msg ) -> Html msg
+mkDropdown modelState openMsg mainTxt itemMsgLst =
+    div []
+        [ Dropdown.dropdown
+            modelState
+            { options = []
+            , toggleMsg = openMsg
+            , toggleButton =
+                Dropdown.toggle [ Button.light ] [ text mainTxt ]
+            , items =
+                List.map
+                    (\( buttonTxt, clickMsg ) ->
+                        Dropdown.buttonItem [ onClick clickMsg ] [ text buttonTxt ]
+                    )
+                    itemMsgLst
+            }
+        ]
