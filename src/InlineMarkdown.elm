4be066c4957d5d6fa772288e06a9855e2ccf6809
Casper Schipper <casper.schipper@gmail.com>
2022-02-13 21:33:53 +0100

    even simpler

--- src/InlineMarkdown.elm
+++ src/InlineMarkdown.elm
@@ -92,8 +92,8 @@ finish str _ =
 escaped : Parser String
 escaped =
     oneOf
-        [ symbol "/*" |> Parser.map (\_ -> "/*")
-        , symbol "/_" |> Parser.map (\_ -> "/_")
+        [ symbol "/*" |> map (\_ -> "/*")
+        , symbol "/_" |> map (\_ -> "/_")
         , problem "expected escaped"
         ]
 
@@ -104,23 +104,22 @@ parseSpan =
         helper : State -> Parser (Step State String)
         helper (State lst) =
             Parser.oneOf
-                [ end |> Parser.map (finish lst)
-                , escaped |> Parser.map (\esc -> Loop (State (esc :: lst)))
+                [ end |> map (finish lst)
+                , escaped |> map (\esc -> Loop (State (esc :: lst)))
                 , inlineSymbols
                     -- we found a markdown symbol !
-                    |> Parser.andThen
+                    |> andThen
                         (\symb ->
                             oneOf
                                 -- It is only valid syntax, if it is not the end, or a space
-                                [ end |> Parser.map (finish lst)
-                                , oneOrMore isSpace |> Parser.map (\spaces -> Loop (State (spaces :: symb :: lst))) -- a space, so it is a literal symb
+                                [ end |> map (finish lst)
+                                , oneOrMore isSpace |> map (\spaces -> Loop (State (spaces :: symb :: lst))) -- a space, so it is a literal symb
                                 , succeed (Loop (State lst)) -- the symb is not included
                                 ]
                         )
 
                 -- now continue, until you find a new markdown symbol or space
-                , oneOrMore (\c -> c /= '*' && c /= '_' && not (isSpace c)) |> getChompedString |> Parser.map (\str -> Loop (State (str :: lst)))
-                , oneOrMore isSpace |> getChompedString |> Parser.map (\str -> Loop (State (str :: lst)))
+                , oneOrMore (\c -> c /= '*' && c /= '_') |> getChompedString |> map (\str -> Loop (State (str :: lst)))
                 ]
 
         start : State
