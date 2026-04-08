d1bff08d0c40ab9f54c76c3cd63a4cf5bf243002
Casper Schipper <casper@researchcatalogue.net>
2026-04-08 09:43:48 +0200

    added build directory

--- build.sh
+++ build.sh
@@ -1,5 +1,30 @@
-elm make --optimize --output=elm.js src/Main.elm
+#!/bin/bash
+set -e
 
-./minify-script.sh src/Main.elm 
+BUILD=build
 
+echo "Creating build directory structure..."
+mkdir -p "$BUILD/lib/icons"
 
+echo "Compiling Elm..."
+elm make --optimize --output="$BUILD/elm.js" src/Main.elm
+
+echo "Copying assets..."
+cp editor.html         "$BUILD/editor.html"
+cp editor.js           "$BUILD/editor.js"
+cp rc-media-widget.js  "$BUILD/rc-media-widget.js"
+cp style.css           "$BUILD/style.css"
+
+cp lib/codemirror.js                    "$BUILD/lib/"
+cp lib/codemirror.css                   "$BUILD/lib/"
+cp lib/markdown.js                      "$BUILD/lib/"
+cp lib/markdown-it.min.js               "$BUILD/lib/"
+cp lib/markdown-it-center-text.min.js   "$BUILD/lib/"
+cp lib/markdown-it-footnote.min.js      "$BUILD/lib/"
+cp lib/markdownit-headers-browser.js    "$BUILD/lib/"
+cp lib/css.js                           "$BUILD/lib/"
+cp lib/morphdom-umd.min.js              "$BUILD/lib/"
+
+cp lib/icons/*.svg "$BUILD/lib/icons/"
+
+echo "Done. Build output in ./$BUILD/"
